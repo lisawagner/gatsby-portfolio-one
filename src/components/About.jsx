8ac7dceb3a853d68e23bb5c3516e9bf0123030ad
Lisa Wagner <lisawagner.online@gmail.com>
2022-04-22 10:41:49 -0700

    code refactor for Contact Form

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaCodepen, FaGithub, FaLinkedinIn  } from 'react-icons/fa'
-import { TiSocialLinkedinCircular } from "react-icons/ti";
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const About = () => {
   return (
@@ -9,18 +9,19 @@ const About = () => {
         <div className="content">
             <h1>Lisa <span className="text-fancy">Wagner</span></h1>
             <div className="subheading mb-3">I'm a <span className="typed" typed-strings="Lifelong Learner, Software Developer, Solutions Creator" ></span></div>
+            <div className="subheading mb-3">I'm a <span className="typed" typed-strings="Lifelong Learner, Software Developer, Solutions Creator" ></span></div>
             <p className="mb-3">I'm a creative coder who enjoys making amazing things with web technologies. I develop modern Web Apps and UI/UX components using React.JS, Node.JS, Redux, Gatsby, and the MERN Stack</p>
           </div>
 
           <ul className="social">
             <li>
-              <a href="#" target="_blank" className="social-item"><FaGithub></FaGithub></a> 
+              <a href="https://github.com/lisawagner"  aria-label="Github link button" rel="noreferrer" target="_blank" className="social-item"><FaGithub></FaGithub></a> 
             </li>
             <li>
-            <a href="#" target="_blank" className="social-item"><FaCodepen></FaCodepen></a>
+            <a href="https://codepen.io/wgnr" aria-label="Codepen link button" rel="noreferrer" target="_blank" className="social-item"><FaCodepen></FaCodepen></a>
             </li>
             <li>
-              <a href="#" target="_blank" className="social-item"><FaLinkedinIn></FaLinkedinIn></a>
+              <a href="https://www.linkedin.com/in/lisa-wagner-87ba929a/" aria-label="LinkedIn link button" rel="noreferrer" target="_blank" className="social-item"><FaLinkedinIn></FaLinkedinIn></a>
             </li>
           </ul>
   
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,18 +15,17 @@ const Contact = () => {
       <form action="" method="POST">
 
         <div className="form-group">
-          <label for="name">Full Name</label>
-          <input type="text" name="Name" id="name" placeholder="First and Last" required minlength="3" maxlength="25" />
+          {/* <label for="name">Full Name</label>
+          <input type="text" name="Name" id="name" placeholder="First and Last" required minlength="3" maxlength="25" /> */}
+          <label for="name">Full Name<input type="text" name="Name" id="name" placeholder="First and Last" required minlength="3" maxlength="25" /></label>
         </div> 
 
         <div className="form-group">
-          <label for="email">Email Address</label>
-          <input type="email" name="Email" id="email" placeholder="email@domain.tld" required />
+          <label for="email">Email Address<input type="email" name="Email" id="email" placeholder="email@domain.tld" required /></label>
         </div> 
 
         <div className="form-group">
-          <label for="message">Message</label>
-          <textarea name="Message" id="message" rows="5" placeholder="Type your message here...."></textarea>
+          <label for="message">Message<textarea name="Message" id="message" rows="5" placeholder="Type your message here...."></textarea></label>       
         </div> 
 
         <div className="form-group">
