dd4300563ea1d2c23bcefdce4ff78432e69d11f4
Lisa Wagner <lisawagner.online@gmail.com>
2022-04-23 09:50:38 -0700

    refactor navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import logo from '../assets/img/logo.png'
 import { GiHamburgerMenu } from "react-icons/Gi"
+import { GrClose } from "react-icons/gr"
 
-const Navbar = ( {toggleSidebar} ) => {
+
+// const Navbar = ( {toggleMenu} ) 
+const Navbar = ( ) => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => setToggle(!toggle);
+
+  const hideMenu = () => setToggle(!toggle); //or !toggle ?
+
   return (
-    <header id="headerMain">
-
-    <div className="profile">
-      <img className="profile-img" src={logo} alt=""/>
-      <h3>Lisa Wagner</h3>
-    </div>
-    <button type="button" id="toggle-btn" onClick={toggleSidebar}><GiHamburgerMenu></GiHamburgerMenu></button>
+    <header id="headerMain" className={toggle ? "active" : ""}>
 
     <nav className="navbar">
-      <ul>
-        <li>
-          <Link className='nav-item' to="#about">about</Link>
+      <div className="profile">
+        <img className="profile-img" src={logo} alt=""/>
+        <h3>Lisa Wagner</h3>
+      </div>
+      <ul >
+        <li >
+          <Link className='nav-item' to="#about" onClick={hideMenu}>about</Link>
         </li>
         <li>
-          <Link className='nav-item' to="#skills">skills</Link>
+          <Link className='nav-item' to="#skills" onClick={hideMenu}>skills</Link>
         </li>
         <li>
-          <Link className='nav-item' to="#portfolio">portfolio</Link>
+          <Link className='nav-item' to="#portfolio" onClick={hideMenu}>portfolio</Link>
         </li>
         <li>
-          <Link className='nav-item' to="#contact">contact</Link>
+          <Link className='nav-item' to="#contact" onClick={hideMenu}>contact</Link>
         </li>
       </ul>
+      <button type="button" id="toggle-btn" onClick={handleToggle}>
+        {!toggle ? (
+          <GiHamburgerMenu></GiHamburgerMenu>
+        ) : (
+            <GrClose></GrClose>
+        )}
+      </button>
     </nav>
 
   </header>
